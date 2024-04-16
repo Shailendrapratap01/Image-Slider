67e4f25a8874dc5fd45d87f1987c33d594e1b5d4
shailendra01 <shailendrapratapsingh@innotechteam.in>
2024-04-16 15:01:22 +0530

    functionality added

--- script.js
+++ script.js
@@ -1,21 +1,60 @@
 const moveNext = document.getElementById("icon2")
 const movePrev = document.getElementById("icon1")
-const img = document.getElementById("img")
-const buttons = document.querySelectorAll("btn")
+const images = document.querySelectorAll(".img")
+const buttons = document.querySelectorAll(".btn")
 
-const imgArr = ["images/img1.jpg", "images/img2.jpg", "images/img3.jpg", "images/img4.jpg", "images/img5.jpg", "images/img6.jpg", "images/img7.jpg", "images/img8.jpg"]
-let n=0;
+let cnt=0;
+
+images.forEach(
+    (image, index) => {
+        image.style.left = `${index * 100}%` ;
+    }
+)
 
 moveNext.addEventListener("click", ()=>{
-    if(n<7){
-        n++;
-        img.src = imgArr[n];
-    }
+    cnt++;
+    moveSlide();
 })
 
 movePrev.addEventListener("click", ()=>{
-    if(n>0){
-        n--;
-        img.src = imgArr[n];
-    }
+    cnt--;
+    moveSlide();
 })
+
+buttons.forEach(
+    (button, index)=>{
+        button.addEventListener("click", ()=>{
+            cnt = index;
+            moveSlide();
+        })
+    }
+)
+
+
+
+const moveSlide = ()=>{
+    images.forEach((image)=>{
+        image.style.transform = `translateX(-${cnt * 100}%)`
+
+        if(cnt===0){
+            movePrev.style.display="none"
+            moveNext.style.display="block"
+        }else if(cnt===7){
+            moveNext.style.display="none"
+        }else{
+            moveNext.style.display="block"
+            movePrev.style.display="block"
+        }
+
+        buttons.forEach((button)=>{
+            button.style.backgroundColor = "white"
+            buttons[cnt].style.backgroundColor = "grey"
+        })
+    })
+}
+
+const slideInterval = setInterval( ()=>{
+    cnt++;
+    if(cnt>7)cnt=0;
+    moveSlide();
+}, 3000);
