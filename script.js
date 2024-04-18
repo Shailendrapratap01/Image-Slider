9cbd6d2495f6eca6e5a5eadd985034e5c3da18d0
shailendra01 <shailendrapratapsingh@innotechteam.in>
2024-04-18 15:49:53 +0530

    functions created

--- script.js
+++ script.js
@@ -25,31 +25,43 @@ let imgArray = [
   "images/img08.jpeg",
 ];
 
-for (let i = 0; i < imgArray.length; i++) {
+const generateAnImage = (url,classList)=>{
   const image = document.createElement("img");
-  image.src = imgArray[i];
-  image.classList.add("newImage");
+  image.src = url;
+  image.classList.add(classList);
   slidingImgContainer.appendChild(image);
 }
+const generateBtn = (classList) =>{
+  const imageSliderBtn = document.createElement("button");
+  imageSliderBtn.classList.add(classList);
+  imgSliderBtnContainer.appendChild(imageSliderBtn);
+}
 
-const generatebtn = () => {
+const hideOrShowArrowBtns=(preArrowDisplay,nextArrowDisplay)=>{
+    arrowPrevBtn.style.display = preArrowDisplay;
+    arrowNextBtn.style.display = nextArrowDisplay;
+}
+
+for (let i = 0; i < imgArray.length; i++) {
+  generateAnImage(imgArray[i], "newImage")
+}
+
   for (let i = 0; i < imgArray.length; i++) {
-    const imageSliderBtn = document.createElement("button");
-    imageSliderBtn.classList.add("new-button");
-    imgSliderBtnContainer.appendChild(imageSliderBtn);
+    generateBtn("new-button")
   }
-};
-generatebtn();
 
-imgSliderBtns = document.querySelectorAll(".new-button");
-imgSliderBtns.forEach((imgSliderBtn, index) => {
-  imgSliderBtn.addEventListener("click", () => {
-    clearInterval(imgSlideInterval);
-    count = index;
-    moveSlide();
-    startImgSlideInterval();
-  });
-});
+navigateDotToImg = ()=>{
+    imgSliderBtns = document.querySelectorAll(".new-button");
+    imgSliderBtns.forEach((imgSliderBtn, index) => {
+    imgSliderBtn.addEventListener("click", () => {
+        clearInterval(imgSlideInterval);
+        count = index;
+        moveSlide();
+        startImgSlideInterval();
+    });
+    });
+}
+navigateDotToImg()
 
 const verifyImageURL = async (url) => {
   url = url?.trim();
@@ -70,29 +82,15 @@ const verifyImageURL = async (url) => {
 
 addSlideBtn.addEventListener("click", async () => {
   clearInterval(imgSlideInterval);
-  let imgUrl = prompt("Enter the image url");
+  const imgUrl = prompt("Enter the image url");
   const verifiedUrl = await verifyImageURL(imgUrl);
   if (verifiedUrl) {
     imgArray.push(imgUrl);
-    const image = document.createElement("img");
-    image.src = imgUrl;
-    image.classList.add("newImage");
-    slidingImgContainer.appendChild(image);
 
-    const imageSliderBtn = document.createElement("button");
-    imageSliderBtn.classList.add("new-button");
-    imgSliderBtnContainer.appendChild(imageSliderBtn);
+    generateAnImage(imgUrl, "newImage")
+    generateBtn("new-button")
+    navigateDotToImg();
 
-    imgSliderBtns = document.querySelectorAll(".new-button");
-
-    imgSliderBtns.forEach((imgSliderBtn, index) => {
-      imgSliderBtn.addEventListener("click", () => {
-        clearInterval(imgSlideInterval);
-        count = index;
-        moveSlide();
-        startImgSlideInterval();
-      });
-    });
   } else if (imgUrl !== null) {
     invalidUrlError.style.display = "flex";
   } else {
@@ -121,24 +119,18 @@ lastImgError.addEventListener("click", () => {
 });
 
 confirmDeleteBtn.addEventListener("click", () => {
-    clearInterval(imgSlideInterval);
+  clearInterval(imgSlideInterval);
   imgArray.splice(count, 1);
   imgArray = imgArray.filter((image) => image);
   const images = document.querySelectorAll(".newImage");
   slidingImgContainer.removeChild(images[count]);
 
   imgSliderBtnContainer.innerHTML = "";
-  generatebtn();
+  for (let i = 0; i < imgArray.length; i++) {
+    generateBtn("new-button")
+  }
 
-  imgSliderBtns = document.querySelectorAll(".new-button");
-  imgSliderBtns.forEach((imgSliderBtn, index) => {
-    imgSliderBtn.addEventListener("click", () => {
-      clearInterval(imgSlideInterval);
-      count = index;
-      moveSlide();
-      startImgSlideInterval();
-    });
-  });
+  navigateDotToImg();
 
   if (count === imgArray.length) {
     count = 0;
@@ -149,7 +141,7 @@ confirmDeleteBtn.addEventListener("click", () => {
     imgSliderBtn.style.backgroundColor = "white";
     imgSliderBtns[count].style.backgroundColor = "grey";
   });
-
+  
   confirmContainer.style.display = "none";
   startImgSlideInterval();
 });
@@ -171,6 +163,11 @@ arrowPrevBtn.addEventListener("click", () => {
 imageContainer.addEventListener("mouseover", () => {
   clearInterval(imgSlideInterval);
 });
+imageContainer.addEventListener("click", () => {
+  invalidUrlError.style.display = "none";
+  lastImgError.style.display = "none";
+  confirmContainer.style.display = "none";
+});
 imageContainer.addEventListener("mouseout", () => {
   startImgSlideInterval();
 });
@@ -179,14 +176,11 @@ const moveSlide = () => {
   slidingImgContainer.style.transform = `translateX(-${100 * count}%)`;
 
   if (count === 0) {
-    arrowPrevBtn.style.display = "none";
-    arrowNextBtn.style.display = "block";
+    hideOrShowArrowBtns("none","block")
   } else if (count === imgArray.length - 1) {
-    arrowNextBtn.style.display = "none";
-    arrowPrevBtn.style.display = "block";
+    hideOrShowArrowBtns("block","none")
   } else {
-    arrowNextBtn.style.display = "block";
-    arrowPrevBtn.style.display = "block";
+    hideOrShowArrowBtns("block","block")
   }
   imgSliderBtns.forEach((imgSliderBtn) => {
     imgSliderBtn.style.backgroundColor = "white";
